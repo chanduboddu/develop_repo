0d8c18821832af654b562e79c73fe318560d8972
chandu <chandu@gmail.com>
2018-08-04 15:18:31 +0530

    commit firdst for jenkin

---
+++ jenkin/jenkin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jenkin.MvcApplication" Language="C#" %>
---
+++ jenkin/jenkin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jenkin.Startup))]
+namespace jenkin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
